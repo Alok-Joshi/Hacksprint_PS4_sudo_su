f158b7c8160bb9cf8f99a738518bb6070c4cfc9a
Alok Joshi <AlokJoshi523@gmail.com>
2022-10-14 14:12:21 +0530

    Fixed bugs. hardcoded server details for now. Will change during deployment

--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from datetime import date
-from sqlalchemy import Column, UniqueConstraint
+from sqlalchemy import Column, ForeignKeyConstraint, UniqueConstraint, create_engine
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer, DateTime
 from sqlalchemy import String
@@ -13,9 +13,9 @@ Base = declarative_base()
 class users(Base):
       __tablename__ = 'users'
 
-      uid = Column(Integer, primar_key = True)
+      uid = Column(Integer, primary_key = True)
       email = Column(String(30), unique = True)
-      password = Column(String(30))
+      password = Column(String(30),nullable = True)
       office_id = Column(Integer, ForeignKey("offices.oid"),nullable = False)
 
 
@@ -37,10 +37,10 @@ class layouts(Base):
 
 class slots(Base):
 
-      __tablename__ = 'layouts'
+      __tablename__ = 'slots'
 
       layout_id = Column(Integer, ForeignKey("layouts.layout_id"),nullable = False,primary_key = True)
-      slot_name = Column(String(30), primary_key = True)
+      slot_name = Column(Integer, primary_key = True,nullable = False)
 
 
 
@@ -49,11 +49,15 @@ class booking(Base):
 
       __tablename__ = 'booking'
 
-      uid = Column(Integer, ForeignKey("users.uid"),nullable = False, unique = True)
-      layout_id = Column(Integer, ForeignKey("layouts.layout_id"),nullable = False)
-      slot_name = Column(Integer, ForeignKey("slots.slot_name"), nullable = False)
+      uid = Column(Integer, ForeignKey("users.uid"),nullable = False, primary_key = True)
+      layout_id = Column(Integer)
+      slot_name = Column(Integer)
       start_time = Column(DateTime)
       end_time = Column(DateTime)
+      __table_args__ = (ForeignKeyConstraint([layout_id,slot_name],[slots.layout_id,slots.slot_name]),{})
 
 
 
+if(__name__  == "__main__"):
+    engine = create_engine("mysql+pymysql://root:password@localhost/feature_db",echo = True, future = True) #TODO: hardcoded for now, shall be changed later
+    Base.metadata.create_all(bind =engine)
